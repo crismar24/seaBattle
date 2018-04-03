7e892faf642cf32925dd26851849e04939feb913
crismar24 <crismar24@gmail.com>
2018-04-04 00:47:50 +0300

    продолжение обучения: инкапсуляция

--- ex4_inc/Date.java
+++ ex4_inc/Date.java
@@ -1,8 +1,33 @@
 package ex4_inc;
 
 public class Date {
-    int day;
-    int mounth;
-    int year;
+    private int day;
+    private int mounth;
+    private int year;
 
+    public int getDay() {
+        return day;
+    }
+
+    public int getMounth() {
+        return mounth;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setDay(int day) {
+        if (day > 0 && day < 32) {
+            this.day = day;
+        }
+    }
+
+    public void setMounth(int mounth) {
+        this.mounth = mounth;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
--- ex4_inc/Main.java
+++ ex4_inc/Main.java
@@ -2,6 +2,8 @@ package ex4_inc;
 
 public class Main {
     public static void main(String[] args) {
-
+        Date date = new Date();
+//        date.day = 32;
+        date.setDay(32);
     }
 }
